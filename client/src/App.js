8246016aadf80c9af9e606099b8c6c303b2b4f49
ethansantalone <ethansantalone@outlook.com>
2022-11-01 19:54:57 -0400

    marijuana and capital punishment links

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,8 @@ const immigrationProps = {
   proLinkNames: ['FWD Economic Impact', 'Economic Policy Institute'],
   neutralLinks: ['https://www.pewresearch.org/fact-tank/2020/08/20/key-findings-about-u-s-immigrants/'],
   neutralLinkNames: ['PEW Research'],
-  conLinks: ['https://www.fairus.org/issues/illegal-immigration?gclid=Cj0KCQjwteOaBhDuARIsADBqRegaG6eTbDV2VpyfUodqy8euvPvQc54GhV2pzpQe9m7SSqngHov1B9saApYKEALw_wcB', 'https://www.fairus.org/issue/illegal-immigration/whats-wrong-illegal-immigration'],
+  conLinks: ['https://www.fairus.org/issues/illegal-immigration?gclid=Cj0KCQjwteOaBhDuARIsADBqRegaG6eTbDV2VpyfUodqy8euvPvQc54GhV2pzpQe9m7SSqngHov1B9saApYKEALw_wcB',
+    'https://www.fairus.org/issue/illegal-immigration/whats-wrong-illegal-immigration'],
   conLinkNames: ['FAIR Fast Facts', 'FAIR Costs of Immigration'],
 };
 const gunControlProps = {
@@ -36,6 +37,29 @@ const gunControlProps = {
   conLinks: ['https://www.americas1stfreedom.org/content/17-inconvenient-facts-for-the-gun-control-movement/', 'https://www.nraila.org/why-gun-control-doesn-t-work/'],
   conLinkNames: ['America\'s 1st Freedom', 'NRA ILA'],
 };
+const marijuanaProps = {
+  proHeader: 'Pro Marijuana Legalization',
+  neutralHeader: 'Neutral',
+  conHeader: 'Anti Marijuana Legalization',
+  proLinks: ['https://www.mpp.org/issues/legalization/top-ten-reasons-to-end-marijuana-prohibition/', 'https://www.investopedia.com/articles/insights/110916/economic-benefits-legalizing-weed.asp'],
+  proLinkNames: ['MPP Legalize and Regulate', 'Investopedia Economic Benefit'],
+  neutralLinks: ['https://marijuana.procon.org/'],
+  neutralLinkNames: ['Procon'],
+  conLinks: ['https://www.liberty.edu/champion/2022/03/pros-and-cons-weed-should-be-illegal/', 'https://www.drugrehab.us/news/why-recreational-marijuana-should-stay-illegal/'],
+  conLinkNames: ['Liberty pros cons', 'Drug Rehab'],
+};
+const capitalPunishmentProps = {
+  proHeader: 'Pro Capital Punishment',
+  neutralHeader: 'Neutral',
+  conHeader: 'Anti Capital Punishment',
+  proLinks: ['https://www.heritage.org/crime-and-justice/commentary/the-death-penalty-appropriate',
+    'https://www.scu.edu/mcae/publications/iie/v1n3/capital.html#:~:text=Capital%20punishment%20is%20often%20defended,killers%20do%20not%20kill%20again.'],
+  proLinkNames: ['Heritage', 'Santa Clara University'],
+  neutralLinks: ['https://deathpenalty.procon.org/'],
+  neutralLinkNames: ['Procon'],
+  conLinks: ['https://www.aclu.org/other/case-against-death-penalty', 'https://www.amnesty.org/en/what-we-do/death-penalty/'],
+  conLinkNames: ['American Civil Liberties Union', 'Amnesty'],
+};
 
 const App = () => {
   const temp = 0;
@@ -46,6 +70,8 @@ const App = () => {
           <Route path="/abortion" element={<Room props={abortionProps} />} />
           <Route path="/guncontrol" element={<Room props={gunControlProps} />} />
           <Route path="/immigration" element={<Room props={immigrationProps} />} />
+          <Route path="/capitalpunishment" element={<Room props={capitalPunishmentProps} />} />
+          <Route path="/marijuana" element={<Room props={marijuanaProps} />} />
         </Routes>
       </Router>
     </div>
